db268becedeaa10c1683121e047029809b2f0943
Sateoo <matteo.salvi@studenti.ittsrimini.edu.it>
2023-05-09 15:17:24 +0200

    Prima Versione

---
+++ Program.cs
@@ -0,0 +1,17 @@
+﻿using SQLite;
+SQLiteConnection cn1= new SQLiteConnection("chinook.db");
+var tblArtist=cn1.Query<Artist>("select * from artists");
+Console.WriteLine($"In questa tabella ci sono{tblArtist.Count} record!");
+
+//stampa della tabella
+foreach(var artista in tblArtist)
+{
+    Console.WriteLine($"{artista.Name}");
+}
+
+
+public class Artist
+{
+    public int ArtistId{get;set;}
+    public string Name{get;set;}
+}
