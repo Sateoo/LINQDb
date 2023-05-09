e9f3e5aa8c6c9e8c5b0b3c54dae459498f46b59f
Sateoo <matteo.salvi@studenti.ittsrimini.edu.it>
2023-05-09 15:58:48 +0200

    Seconda Versione

--- Program.cs
+++ Program.cs
@@ -3,11 +3,21 @@ SQLiteConnection cn1= new SQLiteConnection("chinook.db");
 var tblArtist=cn1.Query<Artist>("select * from artists");
 Console.WriteLine($"In questa tabella ci sono{tblArtist.Count} record!");
 
-//stampa della tabella
+//stampa della tabella dal primo record all'ultimo
 foreach(var artista in tblArtist)
 {
     Console.WriteLine($"{artista.Name}");
 }
+
+//stampa della tabella dall'ultimo record al primo
+//utilizzando  il linguaggio LINQ
+//Language Integrate Query
+tblArtist = tblArtist.OrderByDescending(x=>x.Name).ToList();
+foreach(var artista in tblArtist)
+{
+    Console.WriteLine($"{artista.Name}");
+}
+
 
 
 public class Artist
